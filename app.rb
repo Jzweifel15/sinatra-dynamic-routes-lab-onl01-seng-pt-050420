7bd2eacfbe22084f9d73bbac189817c517de288b
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-20 20:03:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,7 +31,18 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
-    
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    if @operations == "add"
+      "#{@number1 + @number2}"
+    elsif @operations == "subtract"
+      "#{@number1 - @number2}"
+    elsif @operations == "multiply"
+      "#{@number1 * @number2}"
+    elsif @operations == "divide"
+      "#{@number1 / @number2}"
+    end
   end
   
 end
