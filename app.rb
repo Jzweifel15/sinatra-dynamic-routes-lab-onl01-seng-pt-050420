3cee9bcdf2ad1fa8f38324639cadc44ce3dea45d
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-20 19:40:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,13 +4,7 @@ class App < Sinatra::Base
   # Write your code here!
   
   get '/reversename/:name' do
-    @name = param[:name]
-    split_name = @name.split(//)
-    reversed_name = []
-    split_name.length.times do 
-      reversed_name.push(split_name.pop)
-    end
-    reversed_name.join
+ 
   end
   
 end
