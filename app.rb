6e5e9470fd7be2e7d24906bfed67310510f72c12
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-20 19:52:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,7 +22,11 @@ class App < Sinatra::Base
   end
   
   get '/say/:number1/:number2/:number3/:number4/:number5' do
-    
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    @number3 = params[:number3].to_i
+    @number4 = params[:number4].to_i
+    @number5 = params[:number5].to_i
   end
   
 end
